57134494fc054b3155a741b5cd5927abbed7bd85
Ben Woodfield <raser.13@hotmail.com>
2016-12-05 21:58:46 +0000

    wrote a little animation script

--- Dice1_ANIMATION.py
+++ Dice1_ANIMATION.py
@@ -1,12 +1,18 @@
 # Ben Woodfield - DICE ANIMATION
+# So this one is one oof my own
+# This is an animation, you can change the grids to diaplay any images using coloured LED's as pixels
+# Then update the list if you add more images. If you create RGB variables, assign them to a single letter
+# so you can add them to the grid nicely...so far this only has blue and white (b and w)
+#
 # This was a project I started to get Dice simulation using the SenseHat,
-# So I drew out the images using the grids, and have yet do add the random
-
-# What I did manage though was to create an animation where the Hat scrolls
-# Through 6 images (a list of images) and has a small time delay inbetween each one
+# So I drew out the numbered images using the grids, and have yet do add the random capability
+#
+# What I did manage though was to create an animation where the Hat scrolls through a series of
+# 6 images (a list of images) and has a small time delay inbetween each one. You could make a little cartoon
+# The API requires it's own commands, and it's just a case of figuring out how to interact with random
 
 from sense_hat import SenseHat
-import random
+#import random
 import time
 
 sense = SenseHat()
